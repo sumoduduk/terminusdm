a9bbd2a6cf25d9697573449996d60a573bc6a8e0
sumoduduk <sumoduduk13@gmail.com>
2024-03-10 17:16:25 +0700

    fix: using modified trauma crate for concurent download single file

--- src/begin_download.rs
+++ src/begin_download.rs
@@ -1,75 +1,49 @@
-use reqwest::header::HeaderValue;
-use reqwest::{header, Url};
+use reqwest::Url;
 use std::path::PathBuf;
-use std::sync::Arc;
-use trauma::download::Download;
+use trauma::download::{Download, Summary};
 use trauma::downloader::DownloaderBuilder;
 
 use eyre::OptionExt;
 
-pub async fn start_download(uri: &str, range_header: Vec<String>) -> eyre::Result<()> {
-    let download_dirs = dirs::download_dir()
-        .or(Some(PathBuf::from("$HOME/Downloads/")))
-        .ok_or_eyre("Error : No Download Dir")?;
-
-    let arc_temp = Arc::new(download_dirs.join("temp"));
-
-    tokio::fs::create_dir_all(arc_temp.as_ref()).await?;
-
-    let mut handles = Vec::with_capacity(range_header.len());
+pub async fn start_download(
+    temp: PathBuf,
+    uri: &str,
+    range_header: &[(u64, u64)],
+) -> eyre::Result<Vec<Summary>> {
+    tokio::fs::create_dir_all(&temp).await?;
 
     let url = Url::parse(uri)?;
 
-    let arc_url = Arc::new(url);
+    let mut batch_dl = Vec::with_capacity(range_header.len());
 
-    for (i, range) in range_header.into_iter().enumerate() {
-        let url = Arc::clone(&arc_url);
-        let temp = Arc::clone(&arc_temp);
-
-        let handle = tokio::spawn(multi_downoad(temp, i, url, range));
-
-        handles.push(handle);
+    for (i, range) in range_header.iter().enumerate() {
+        let dl = Download::new(&url, &i.to_string(), Some(*range));
+        batch_dl.push(dl);
     }
 
-    // for handle in handles {
-    //     handles.aw
-    // }
+    let begin = DownloaderBuilder::new().directory(temp).build();
 
-    Ok(())
-}
-async fn multi_downoad(temp: Arc<PathBuf>, i: usize, url: Arc<Url>, range: String) {
-    let filename_str = temp
-        .as_os_str()
-        .to_str()
-        .ok_or_eyre("Failed to parse os str");
+    let summary = begin.download(&batch_dl).await;
 
-    if let Ok(filename) = filename_str {
-        let filename = format!("{filename}/{i}");
-
-        let dl = Download::new(url.as_ref(), &filename);
-        let header_val = HeaderValue::from_str(&range);
-
-        if let Ok(header_val) = header_val {
-            let builder = DownloaderBuilder::new()
-                .header(header::RANGE, header_val)
-                .build();
-            let dl = &[dl];
-
-            // let _ = builder.download(dl).await;
-        };
-    };
+    Ok(summary)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{req_lib::HeaderObject, utils::create_range};
+    use crate::{merge_file::merge, req_lib::HeaderObject, utils::create_range};
     use eyre::{eyre, OptionExt};
 
     const URI : &str = "https://huggingface.co/datasets/ym0v0my/Time_series_dataset/resolve/main/all_six_datasets.zip?download=true";
 
     #[tokio::test]
     async fn test_concurent_donwload() -> eyre::Result<()> {
+        let download_dirs = dirs::download_dir()
+            .or(Some(PathBuf::from("test/Downloads/")))
+            .ok_or_eyre("Error : No Download Dir")?;
+
+        let temp = download_dirs.join("temp");
+
         let header_obj = HeaderObject::new(&URI).await?;
         if !header_obj.is_ranges()? {
             return Err(eyre!("Not Resumable"));
@@ -77,12 +51,20 @@ mod tests {
 
         let sizes = header_obj.get_sizes()?;
         dbg!(sizes);
+        let divisor = 8;
 
-        let ranges = create_range(sizes, 8).ok_or_eyre("Error: divisor should be non-zero")?;
+        let ranges =
+            create_range(sizes, divisor).ok_or_eyre("Error: divisor should be non-zero")?;
+        let out_file = "output.zip";
 
-        let summary = start_download(&URI, ranges).await;
-        dbg!(&summary);
+        let summary = start_download(temp.clone(), &URI, &ranges).await;
         assert!(summary.is_ok());
+
+        let res = merge(&temp, divisor as usize, out_file).await;
+
+        dbg!(&res);
+        assert!(res.is_ok());
+
         Ok(())
     }
 }
