f4d2d67835191ec9714645b2218e42dc2d589912
sumoduduk <sumoduduk13@gmail.com>
2024-03-28 14:56:09 +0700

    feat: create tabs state field

---
+++ src/tui/app/tabs_state.rs
@@ -0,0 +1,46 @@
+use ratatui::{
+    style::{palette::tailwind, Stylize},
+    text::Line,
+};
+use strum::{Display, EnumIter, FromRepr};
+
+#[derive(Default, FromRepr, EnumIter, Display, Clone, Copy)]
+pub enum SelectedTabs {
+    #[default]
+    DownloadFolder,
+    ConcurrentTotal,
+    ChunkSize,
+    Language,
+}
+
+impl SelectedTabs {
+    pub fn prev(self) -> Self {
+        let curr_idx: usize = self as usize;
+        let index = curr_idx.saturating_sub(1);
+
+        Self::from_repr(index).unwrap_or(self)
+    }
+
+    pub fn next(self) -> Self {
+        let curr_idx: usize = self as usize;
+        let index = curr_idx.saturating_add(1);
+
+        Self::from_repr(index).unwrap_or(self)
+    }
+
+    pub fn title(self) -> Line<'static> {
+        format!("  {self}  ")
+            .fg(tailwind::SLATE.c200)
+            .bg(self.palette().c900)
+            .into()
+    }
+
+    pub const fn palette(self) -> tailwind::Palette {
+        match self {
+            Self::DownloadFolder => tailwind::BLUE,
+            Self::ConcurrentTotal => tailwind::EMERALD,
+            Self::ChunkSize => tailwind::INDIGO,
+            Self::Language => tailwind::RED,
+        }
+    }
+}
