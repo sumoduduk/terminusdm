39f0589fd5984c07389ea1fd7758d234d18c0766
sumoduduk <sumoduduk13@gmail.com>
2024-03-30 03:07:36 +0700

    feat: add handle tabs event to change config

--- src/tui/event_tui/event_tab.rs
+++ src/tui/event_tui/event_tab.rs
@@ -8,19 +8,23 @@ use crate::tui::app::CurrentScreen;
 use crate::tui::app::InputMode;
 use tui_input::backend::crossterm::EventHandler;
 
-pub fn handle_tabs_event(app: &mut AppTui) {
+pub fn handle_tabs_event(app: &mut AppTui, key: KeyEvent) {
     match app.selected_tabs {
-        SelectedTabs::DownloadFolder => todo!(),
-        SelectedTabs::ConcurrentTotal => todo!(),
-        SelectedTabs::ChunkSize => todo!(),
-        SelectedTabs::Language => todo!(),
+        SelectedTabs::Language => match key.code {
+            KeyCode::Tab => app.curr_screen = CurrentScreen::Main,
+            KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
+            KeyCode::Char('l') | KeyCode::Right => app.next_tab(),
+            KeyCode::Char('h') | KeyCode::Left => app.previous_tab(),
+            _ => {}
+        },
+        _ => handle_tabs_content(app, key),
     }
 }
 
-fn handle_tabs_download(app: &mut AppTui) {
-    match app.input_mode {
-        InputMode::Normal => todo!(),
-        InputMode::Editing => todo!(),
+fn handle_tabs_content(app: &mut AppTui, key: KeyEvent) {
+    match app.tab_content_mode {
+        InputMode::Normal => handle_input_normal(app, key),
+        InputMode::Editing => handle_input_insert(app, key),
     }
 }
 
@@ -29,8 +33,7 @@ fn handle_input_normal(app: &mut AppTui, key: KeyEvent) {
         KeyCode::Tab => app.curr_screen = CurrentScreen::Main,
         KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
         KeyCode::Char('e') => {
-            todo!();
-            app.input_mode = InputMode::Editing;
+            app.tab_content_mode = InputMode::Editing;
         }
         KeyCode::Char('l') | KeyCode::Right => app.next_tab(),
         KeyCode::Char('h') | KeyCode::Left => app.previous_tab(),
@@ -41,14 +44,22 @@ fn handle_input_normal(app: &mut AppTui, key: KeyEvent) {
 fn handle_input_insert(app: &mut AppTui, key: KeyEvent) {
     match key.code {
         KeyCode::Tab => {
-            app.input_mode = InputMode::Normal;
+            app.tab_content_input.reset();
+            app.tab_content_mode = InputMode::Normal;
             app.curr_screen = CurrentScreen::Main
         }
+        KeyCode::Esc => {
+            app.tab_content_input.reset();
+            app.tab_content_mode = InputMode::Normal;
+        }
         KeyCode::Enter => {
-            todo!();
+            let value = app.tab_content_input.value().to_string();
+            app.update_config(&value, None);
+            app.tab_content_input.reset();
+            app.tab_content_mode = InputMode::Normal;
         }
         _ => {
-            app.tabs_content().handle_event(&Event::Key(key));
+            app.tab_content_input.handle_event(&Event::Key(key));
         }
     }
 }
