5fc2788bc43cb62840e2b1ca697337d1b5eb0e83
sumoduduk <sumoduduk13@gmail.com>
2024-03-30 03:07:00 +0700

    feat: render input setting config

--- src/tui/main_layout/tabs_layout.rs
+++ src/tui/main_layout/tabs_layout.rs
@@ -1,13 +1,15 @@
 use ratatui::{
-    layout::{Alignment, Rect},
+    layout::{Alignment, Constraint, Layout, Margin, Rect},
     style::{Color, Style},
     symbols,
+    text::{Line, Span},
     widgets::{block::Title, Block, BorderType, Borders, Padding, Paragraph, Tabs},
     Frame,
 };
 use strum::IntoEnumIterator;
 
 use crate::tui::app::{tabs_state::SelectedTabs, AppTui, CurrentScreen};
+use crate::{config::Language, tui::app::InputMode::*};
 
 pub fn render_tabs(frame: &mut Frame, app: &mut AppTui, area: Rect) {
     let title_tab = SelectedTabs::iter().map(SelectedTabs::title);
@@ -25,31 +27,101 @@ pub fn render_tabs(frame: &mut Frame, app: &mut AppTui, area: Rect) {
     frame.render_widget(tabs_header, area)
 }
 
-fn render_tab0(app: &AppTui) -> Paragraph<'static> {
-    Paragraph::new("Download Folder").block(block(app))
+pub fn render_tabs_content(frame: &mut Frame, app: &AppTui, area: Rect) {
+    let block_content = block(app);
+    frame.render_widget(block_content, area);
+    let inside_area = area.inner(&Margin {
+        horizontal: 1,
+        vertical: 1,
+    });
+
+    let inside_rect = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Length(1),
+        Constraint::Fill(1),
+    ]);
+    let [_, content_layout, input_layout] = inside_rect.areas(inside_area);
+
+    let config = &app.setting;
+
+    match app.selected_tabs {
+        SelectedTabs::DownloadFolder => {
+            let content = config.default_folder.display().to_string();
+
+            span_content("Default Folder :", &content, content_layout, frame, app);
+            render_value_input(app, frame, input_layout);
+        }
+        SelectedTabs::ConcurrentTotal => {
+            let content = config.concurrent_download.to_string();
+            span_content(
+                "Number of Concurrent Download :",
+                &content,
+                content_layout,
+                frame,
+                app,
+            );
+
+            render_value_input(app, frame, input_layout);
+        }
+        SelectedTabs::ChunkSize => {
+            let content = config.total_chunk.to_string();
+            span_content(
+                "Total Chunk per File :",
+                &content,
+                content_layout,
+                frame,
+                app,
+            );
+
+            render_value_input(app, frame, input_layout);
+        }
+        _ => {}
+    };
 }
 
-fn render_tab1(app: &AppTui) -> Paragraph<'static> {
-    Paragraph::new("Concurrent Total").block(block(app))
+fn render_value_input(app: &AppTui, frame: &mut Frame, area: Rect) {
+    let value = app.tab_content_input.value();
+    let border = Block::default()
+        .borders(Borders::ALL)
+        .title("Enter new value")
+        .border_style(app.selected_tabs.palette().c400)
+        .border_type(BorderType::Rounded);
+
+    let input_par = Paragraph::new(value).block(border);
+    let width = area.width.max(3) - 3;
+    let scroll_input = app.tab_content_input.visual_scroll(width as usize);
+
+    frame.render_widget(input_par, area);
+
+    match app.tab_content_mode {
+        Normal => {}
+        Editing => frame.set_cursor(
+            area.x
+                + ((app.tab_content_input.visual_cursor().max(scroll_input) - scroll_input) as u16
+                    + 1),
+            area.y + 1,
+        ),
+    }
 }
 
-fn render_tab2(app: &AppTui) -> Paragraph<'static> {
-    Paragraph::new("Chunk Size").block(block(app))
-}
+fn span_content(key: &str, val: &str, area: Rect, frame: &mut Frame, app: &AppTui) {
+    let area = area.inner(&Margin {
+        horizontal: 1,
+        vertical: 0,
+    });
+    let value_layout = Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)]);
+    let [left_side, right_side] = value_layout.areas(area);
 
-fn render_tab3(app: &AppTui) -> Paragraph<'static> {
-    Paragraph::new("Language").block(block(app))
-}
+    let left_key = Line::from(key)
+        .centered()
+        .style(Style::default().bg(app.selected_tabs.palette().c700));
 
-/// A block surrounding the tab's content
-fn block(app: &AppTui) -> Block<'static> {
-    Block::default()
-        .title(
-            Title::from("◄ ► to change tab | Press e to edit | Enter to confirm")
-                .alignment(Alignment::Right),
-        )
-        .borders(Borders::TOP)
-        .border_style(app.selected_tabs.palette().c700)
+    frame.render_widget(left_key, left_side);
+    let right_val = Line::from(val)
+        .centered()
+        .style(Style::default().bg(app.selected_tabs.palette().c500));
+
+    frame.render_widget(right_val, right_side);
 }
 
 pub fn outer_block_setting(app: &AppTui) -> Block<'static> {
@@ -66,13 +138,14 @@ pub fn outer_block_setting(app: &AppTui) -> Block<'static> {
         })
 }
 
-pub fn render_tabs_content(frame: &mut Frame, app: &AppTui, area: Rect) {
-    let content = match app.selected_tabs {
-        SelectedTabs::DownloadFolder => render_tab0(app),
-        SelectedTabs::ConcurrentTotal => render_tab1(app),
-        SelectedTabs::ChunkSize => render_tab2(app),
-        SelectedTabs::Language => render_tab3(app),
+fn block(app: &AppTui) -> Block<'static> {
+    let title_tab_content = match app.tab_content_mode {
+        Normal => "◄ ► to change tab | Press e to edit ",
+        Editing => "Press Esc to cancel | Enter to confirm",
     };
 
-    frame.render_widget(content, area);
+    Block::default()
+        .title(Title::from(title_tab_content).alignment(Alignment::Right))
+        .borders(Borders::TOP)
+        .border_style(app.selected_tabs.palette().c500)
 }
