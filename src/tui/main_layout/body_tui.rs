f62d9507f5bb0bde5da661ef54a562297e91c85b
sumoduduk <sumoduduk13@gmail.com>
2024-04-01 05:16:04 +0700

    chore: remove unused function

--- src/tui/main_layout/body_tui.rs
+++ src/tui/main_layout/body_tui.rs
@@ -6,33 +6,6 @@ use ratatui::{
 
 use crate::tui::app::{AppTui, CurrentScreen, InputMode};
 
-pub fn body_comp(app: &AppTui) -> List<'static> {
-    let block_body = Block::default()
-        .title("Download History")
-        .borders(Borders::ALL)
-        .border_style(match app.curr_screen {
-            CurrentScreen::Main => Style::default().fg(Color::Cyan),
-            _ => Style::default(),
-        })
-        .border_type(match app.curr_screen {
-            CurrentScreen::Main => BorderType::Thick,
-            _ => BorderType::Rounded,
-        });
-
-    let mut list_item: Vec<ListItem> = Vec::new();
-
-    for name_input in &app.saved_input {
-        list_item.push(ListItem::new(Line::from(Span::styled(
-            format!("{: <25}", name_input),
-            Style::default().fg(Color::Yellow),
-        ))))
-    }
-
-    let list = List::new(list_item).block(block_body);
-
-    list
-}
-
 pub fn input_editing(app: &AppTui, width: u16) -> Paragraph<'static> {
     let scroll_input = app.input_uri.visual_scroll(width as usize);
 
