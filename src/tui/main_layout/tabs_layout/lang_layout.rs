6a49d9a078e83cb1b54b92bcc020c208ca7e1587
sumoduduk <sumoduduk13@gmail.com>
2024-04-02 03:24:47 +0700

    chore: remove unnecessary argumanet and import

--- src/tui/main_layout/tabs_layout/lang_layout.rs
+++ src/tui/main_layout/tabs_layout/lang_layout.rs
@@ -1,14 +1,13 @@
 use ratatui::{
-    layout::{Constraint, Layout, Rect},
+    layout::Rect,
     style::{Modifier, Style},
-    widgets::{HighlightSpacing, List, Paragraph},
+    widgets::{HighlightSpacing, List},
     Frame,
 };
 
-use crate::config::{self, Language};
 use crate::tui::app::AppTui;
 
-pub fn render_lang_layout(frame: &mut Frame, app: &mut AppTui, area: Rect, index: usize) {
+pub fn render_lang_layout(frame: &mut Frame, app: &mut AppTui, area: Rect) {
     let list = List::new(["English", "Indonesia"])
         .highlight_style(Style::default().add_modifier(Modifier::REVERSED))
         .highlight_symbol(">>  ")
