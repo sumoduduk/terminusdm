304dcd5195d137e5bfc3c1f5b9e6e94f5019309e
sumoduduk <sumoduduk13@gmail.com>
2024-04-02 03:31:32 +0700

    feat: add multilanguage in body layout

--- src/tui/main_layout/body_tui.rs
+++ src/tui/main_layout/body_tui.rs
@@ -1,18 +1,25 @@
 use ratatui::{
     layout::Alignment,
     style::{Color, Style},
-    text::{Line, Span, Text},
-    widgets::{block::Title, Block, BorderType, Borders, List, ListItem, Paragraph, Wrap},
+    text::Text,
+    widgets::{block::Title, Block, BorderType, Borders, Paragraph, Wrap},
 };
 
-use crate::tui::app::{AppTui, CurrentScreen, InputMode};
+use crate::{
+    tui::app::{AppTui, CurrentScreen},
+    words::WORDS,
+};
 
 pub fn input_editing(app: &AppTui, width: u16) -> Paragraph<'static> {
     let scroll_input = app.input_uri.visual_scroll(width as usize);
+    let lang = &app.setting.language;
+
+    let input_title = WORDS::InputTitle;
+    let input_nav = WORDS::InputNavigation;
 
     let input_components = Block::default()
-        .title("Enter a URL")
-        .title(Title::from("Press ENTER to download | ESC to quit").alignment(Alignment::Right))
+        .title(input_title.load_text(lang))
+        .title(Title::from(input_nav.load_text(lang)).alignment(Alignment::Right))
         .borders(Borders::ALL)
         .border_style(match app.curr_screen {
             CurrentScreen::Editing => Style::default().fg(Color::Cyan),
@@ -26,40 +33,24 @@ pub fn input_editing(app: &AppTui, width: u16) -> Paragraph<'static> {
     let value = app.input_uri.value();
 
     let input_par = Paragraph::new(value.to_string())
-        // .style(match app.input_mode {
-        //     InputMode::Normal => Style::default(),
-        //     InputMode::Editing => Style::default().fg(Color::Magenta),
-        // })
         .block(input_components)
         .scroll((0, scroll_input as u16));
 
     input_par
 }
 
-pub fn user_settings(app: &AppTui) -> Block<'static> {
-    Block::default()
-        .title("Setting")
-        .borders(Borders::ALL)
-        .border_style(match app.curr_screen {
-            CurrentScreen::Setting => Style::default().fg(Color::Cyan),
-            _ => Style::default(),
-        })
-        .border_type(match app.curr_screen {
-            CurrentScreen::Setting => BorderType::Thick,
-            _ => BorderType::Rounded,
-        })
-}
+pub fn popup_exit(app: &AppTui) -> Paragraph<'static> {
+    let lang = &app.setting.language;
 
-pub fn popup_exit() -> Paragraph<'static> {
+    let input_title = WORDS::ExitTitle;
+    let input_nav = WORDS::ExitContent;
+
     let popup_exit_component = Block::default()
-        .title("Press Y to confirm/N to cancel")
+        .title(input_title.load_text(lang))
         .borders(Borders::NONE)
         .style(Style::default().bg(Color::DarkGray));
 
-    let exit_text = Text::styled(
-        "Would you like to quit? (y/n)",
-        Style::default().fg(Color::Red),
-    );
+    let exit_text = Text::styled(input_nav.load_text(lang), Style::default().fg(Color::Red));
     let exit_paragraph = Paragraph::new(exit_text)
         .block(popup_exit_component)
         .wrap(Wrap { trim: false });
