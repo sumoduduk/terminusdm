35b13030932f38d5fe16906219de8f0a16c79833
sumoduduk <sumoduduk13@gmail.com>
2024-04-02 03:23:28 +0700

    feat: add multi language in footer

---
+++ src/tui/main_layout/footer_tui/text_footer.rs
@@ -0,0 +1,41 @@
+use crate::config::Language;
+
+pub enum TextFooter {
+    SpanEditing,
+    NoteMain,
+    NoteEditing,
+    NoteSetting,
+    NoteBase,
+}
+
+use Language::*;
+use TextFooter::*;
+
+impl TextFooter {
+    pub fn load_text(&self, language: &Language) -> String {
+        let text = match self {
+            SpanEditing => match language {
+                English => "Press ENTER to Re-Download",
+                Indonesia => "Tekan ENTER untuk mengunduh ulang",
+            },
+            NoteMain => match language {
+                English => "(q) to quit/Tab to switch/Space to select URL",
+                Indonesia => "(q) untuk keluar/Tab untuk berpindah mode",
+            },
+            NoteEditing => match language {
+                English => "(ESC) to cancel/(Tab) to switch boxes/ Enter to complete",
+                Indonesia => "(ESC) untuk keluar/(Tab) untuk berpindah mode",
+            },
+            NoteSetting => match language {
+                English => "(q) to quit/(Tab) to switch boxes",
+                Indonesia => "(q) untuk keluar/Tab untuk berpindah mode",
+            },
+            NoteBase => match language {
+                English => "(q) to quit",
+                Indonesia => "(q) untuk keluar",
+            },
+        };
+
+        text.to_owned()
+    }
+}
