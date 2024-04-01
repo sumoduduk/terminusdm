6bdba890b5332223ec01523c5c03d701da1bad74
sumoduduk <sumoduduk13@gmail.com>
2024-04-02 03:31:16 +0700

    feat: add multi language in error popup

--- src/tui/main_layout/error_popup.rs
+++ src/tui/main_layout/error_popup.rs
@@ -1,14 +1,17 @@
 use ratatui::{
-    style::{Color, Style, Stylize},
+    style::{Color, Style},
     text::Text,
     widgets::{Block, BorderType, Borders, Paragraph, Wrap},
 };
 
-use crate::tui::app::AppTui;
+use crate::{tui::app::AppTui, words::WORDS};
 
 pub fn popup_error(app: &AppTui) -> Paragraph<'static> {
+    let language = &app.setting.language;
+    let error_popup = WORDS::ErrorPopup;
+
     let popup_exit_component = Block::default()
-        .title("Press Enter to continue")
+        .title(error_popup.load_text(language))
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded)
         .style(Style::default().bg(Color::Red).fg(Color::White));
