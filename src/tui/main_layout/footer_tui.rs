a487829bc71ffec8bb33843bc839b702fab36f25
sumoduduk <sumoduduk13@gmail.com>
2024-04-01 22:26:24 +0700

    chore : make message key more concise

--- src/tui/main_layout/footer_tui.rs
+++ src/tui/main_layout/footer_tui.rs
@@ -9,7 +9,9 @@ use crate::tui::app::{AppTui, CurrentScreen};
 pub fn footer_comp_mode(app: &AppTui) -> Paragraph<'static> {
     let cur_span = match app.curr_screen {
         CurrentScreen::Main => Span::styled("Table Mode", Style::default().fg(Color::Green)),
-        CurrentScreen::Editing => Span::styled("Editing Mode", Style::default().fg(Color::Yellow)),
+        CurrentScreen::Editing => {
+            Span::styled("Enter URL Mode", Style::default().fg(Color::Yellow))
+        }
         CurrentScreen::Setting => Span::styled("Setting Mode", Style::default().fg(Color::Blue)),
         CurrentScreen::Exiting => Span::styled("Exiting", Style::default().fg(Color::LightRed)),
         CurrentScreen::PrepareDownload => {
@@ -26,13 +28,11 @@ pub fn footer_comp_mode(app: &AppTui) -> Paragraph<'static> {
     let divider_span = Span::styled(" | ", Style::default().fg(Color::White));
 
     let is_editing_span = match app.curr_screen {
-        CurrentScreen::Editing => {
-            Span::styled("Editing Json Key", Style::default().fg(Color::Green))
-        }
-        _ => Span::styled(
-            "Not Editing Anything",
-            Style::default().fg(Color::LightGreen),
+        CurrentScreen::Main => Span::styled(
+            "Press ENTER to Re-Download",
+            Style::default().fg(Color::Green),
         ),
+        _ => Span::styled(" ", Style::default().fg(Color::LightGreen)),
     };
 
     let curr_navigation_text = vec![cur_span, divider_span, is_editing_span];
@@ -47,15 +47,15 @@ pub fn footer_comp_notes(app: &AppTui) -> Paragraph<'static> {
     let current_keys_hint = {
         match app.curr_screen {
             CurrentScreen::Main => Span::styled(
-                "(q) to quit / Tab to switch",
+                "(q) to quit/Tab to switch/Space to select URL",
                 Style::default().fg(Color::LightRed),
             ),
             CurrentScreen::Editing => Span::styled(
-                "(ESC) to cancel/(Tab) to switch boxes/enter to complete",
+                "(ESC) to cancel/(Tab) to switch boxes/ Enter to complete",
                 Style::default().fg(Color::LightBlue),
             ),
             CurrentScreen::Setting => Span::styled(
-                "(q) to quit / (Tab) to switch boxes/enter to complete",
+                "(q) to quit/(Tab) to switch boxes",
                 Style::default().fg(Color::LightBlue),
             ),
             _ => Span::styled("(q) to quit", Style::default().fg(Color::LightGreen)),
