0cefd7486e3216afed06730fcd64200f11328d83
sumoduduk <sumoduduk13@gmail.com>
2024-04-02 18:04:16 +0700

    fix : remove event poll

--- src/tui/event_tui.rs
+++ src/tui/event_tui.rs
@@ -18,110 +18,108 @@ pub async fn run_app<B: Backend>(
     loop {
         terminal.draw(|f| ui(f, app))?;
 
-        if crossterm::event::poll(std::time::Duration::from_millis(16))? {
-            if let Event::Key(key) = event::read()? {
-                if key.kind == event::KeyEventKind::Release {
-                    continue;
-                }
-                match app.curr_screen {
-                    CurrentScreen::Main => match key.code {
-                        KeyCode::Tab => app.curr_screen = CurrentScreen::Editing,
-                        KeyCode::Char(' ') => app.table.pick(),
-                        KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
-                        KeyCode::Char('j') | KeyCode::Down => app.table.next(),
-                        KeyCode::Char('k') | KeyCode::Up => app.table.previous(),
-                        KeyCode::Enter => {
-                            if !app.table.picked.is_empty() {
-                                app.saved_input.clear();
-                                app.load_pick();
-                                app.curr_screen = CurrentScreen::Download;
-                            }
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                continue;
+            }
+            match app.curr_screen {
+                CurrentScreen::Main => match key.code {
+                    KeyCode::Tab => app.curr_screen = CurrentScreen::Editing,
+                    KeyCode::Char(' ') => app.table.pick(),
+                    KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
+                    KeyCode::Char('j') | KeyCode::Down => app.table.next(),
+                    KeyCode::Char('k') | KeyCode::Up => app.table.previous(),
+                    KeyCode::Enter => {
+                        if !app.table.picked.is_empty() {
+                            app.saved_input.clear();
+                            app.load_pick();
+                            app.curr_screen = CurrentScreen::Download;
                         }
-                        _ => {}
-                    },
-                    CurrentScreen::Setting => {
-                        handle_tabs_event(app, key);
                     }
-                    CurrentScreen::PrepareDownload => match key.code {
-                        KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
-                        KeyCode::Enter | KeyCode::Char('y') => match app.save_input().await {
-                            Ok(_) => {
-                                app.input_uri.reset();
-                                app.save_history()?;
-                                return Ok(true);
-                            }
-                            Err(err) => {
-                                app.set_error_msg(err.to_string());
-                                app.input_uri.reset();
-                            }
-                        },
-                        _ => {}
-                    },
-                    CurrentScreen::Download => match key.code {
-                        KeyCode::Char('y') | KeyCode::Enter => return Ok(true),
-                        KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
-                        KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
-                        _ => {}
-                    },
-                    CurrentScreen::Exiting => match key.code {
-                        KeyCode::Char('y') => return Ok(false),
-                        KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
-                        _ => (),
-                    },
-                    CurrentScreen::Editing => match key.code {
-                        KeyCode::Enter => {
-                            if !app.input_uri.value().is_empty() {
-                                app.clear_saved_input();
-                                app.curr_screen = CurrentScreen::PrepareDownload;
-                            }
-                        }
-                        KeyCode::Esc => {
-                            app.input_uri.reset();
-                            app.curr_screen = CurrentScreen::Exiting;
-                        }
-                        KeyCode::Tab => app.curr_screen = CurrentScreen::Setting,
-                        _ => {
-                            app.input_uri.handle_event(&Event::Key(key));
-                        }
-                    },
-                    // CurrentScreen::Editing => match app.input_mode {
-                    //     InputMode::Normal => match key.code {
-                    //         KeyCode::Char('i') => app.input_mode = InputMode::Editing,
-                    //         KeyCode::Tab => app.curr_screen = CurrentScreen::Setting,
-                    //         _ => {}
-                    //     },
-                    //     InputMode::Editing => match (key.modifiers, key.code) {
-                    //         (modifiers, KeyCode::Enter) => match modifiers {
-                    //             KeyModifiers::CONTROL => app.push_to_table().await,
-                    //             KeyModifiers::NONE => {
-                    //                 if app.input_uri.value().len() > 0 {
-                    //                     app.clear_saved_input();
-                    //                     app.input_mode = InputMode::Normal;
-                    //                     app.curr_screen = CurrentScreen::PrepareDownload;
-                    //                 }
-                    //             }
-                    //             _ => {}
-                    //         },
-                    //         (KeyModifiers::NONE, KeyCode::Esc) => {
-                    //             app.input_mode = InputMode::Normal
-                    //         }
-                    //         (KeyModifiers::NONE, KeyCode::Tab) => {
-                    //             app.input_mode = InputMode::Normal;
-                    //             app.curr_screen = CurrentScreen::Setting;
-                    //         }
-                    //         _ => {
-                    //             app.input_uri.handle_event(&Event::Key(key));
-                    //         }
-                    //     },
-                    // },
-                    CurrentScreen::ErrorScreen => match key.code {
-                        KeyCode::Enter => {
-                            app.clear_error_msg();
-                            app.curr_screen = CurrentScreen::Main;
-                        }
-                        _ => {}
-                    },
+                    _ => {}
+                },
+                CurrentScreen::Setting => {
+                    handle_tabs_event(app, key);
                 }
+                CurrentScreen::PrepareDownload => match key.code {
+                    KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
+                    KeyCode::Enter | KeyCode::Char('y') => match app.save_input().await {
+                        Ok(_) => {
+                            app.input_uri.reset();
+                            app.save_history()?;
+                            return Ok(true);
+                        }
+                        Err(err) => {
+                            app.set_error_msg(err.to_string());
+                            app.input_uri.reset();
+                        }
+                    },
+                    _ => {}
+                },
+                CurrentScreen::Download => match key.code {
+                    KeyCode::Char('y') | KeyCode::Enter => return Ok(true),
+                    KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
+                    KeyCode::Char('q') => app.curr_screen = CurrentScreen::Exiting,
+                    _ => {}
+                },
+                CurrentScreen::Exiting => match key.code {
+                    KeyCode::Char('y') | KeyCode::Enter => return Ok(false),
+                    KeyCode::Char('n') => app.curr_screen = CurrentScreen::Main,
+                    _ => (),
+                },
+                CurrentScreen::Editing => match key.code {
+                    KeyCode::Enter => {
+                        if !app.input_uri.value().is_empty() {
+                            app.clear_saved_input();
+                            app.curr_screen = CurrentScreen::PrepareDownload;
+                        }
+                    }
+                    KeyCode::Esc => {
+                        app.input_uri.reset();
+                        app.curr_screen = CurrentScreen::Exiting;
+                    }
+                    KeyCode::Tab => app.curr_screen = CurrentScreen::Setting,
+                    _ => {
+                        app.input_uri.handle_event(&Event::Key(key));
+                    }
+                },
+                // CurrentScreen::Editing => match app.input_mode {
+                //     InputMode::Normal => match key.code {
+                //         KeyCode::Char('i') => app.input_mode = InputMode::Editing,
+                //         KeyCode::Tab => app.curr_screen = CurrentScreen::Setting,
+                //         _ => {}
+                //     },
+                //     InputMode::Editing => match (key.modifiers, key.code) {
+                //         (modifiers, KeyCode::Enter) => match modifiers {
+                //             KeyModifiers::CONTROL => app.push_to_table().await,
+                //             KeyModifiers::NONE => {
+                //                 if app.input_uri.value().len() > 0 {
+                //                     app.clear_saved_input();
+                //                     app.input_mode = InputMode::Normal;
+                //                     app.curr_screen = CurrentScreen::PrepareDownload;
+                //                 }
+                //             }
+                //             _ => {}
+                //         },
+                //         (KeyModifiers::NONE, KeyCode::Esc) => {
+                //             app.input_mode = InputMode::Normal
+                //         }
+                //         (KeyModifiers::NONE, KeyCode::Tab) => {
+                //             app.input_mode = InputMode::Normal;
+                //             app.curr_screen = CurrentScreen::Setting;
+                //         }
+                //         _ => {
+                //             app.input_uri.handle_event(&Event::Key(key));
+                //         }
+                //     },
+                // },
+                CurrentScreen::ErrorScreen => match key.code {
+                    KeyCode::Enter => {
+                        app.clear_error_msg();
+                        app.curr_screen = CurrentScreen::Main;
+                    }
+                    _ => {}
+                },
             }
         }
     }
