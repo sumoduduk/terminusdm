ee51d073f2d8a6c817249a326b094bce25fa2992
sumoduduk <sumoduduk13@gmail.com>
2024-06-14 22:37:11 +0700

    chore: use external sorting file function

--- src/merge_file.rs
+++ src/merge_file.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
 
+use crate::sort_files::sorting;
 use crate::utils::fs_utils::get_available_filename;
 
 pub async fn merge(
@@ -12,30 +13,8 @@ pub async fn merge(
     output_name: &str,
 ) -> eyre::Result<()> {
     println!("INFO: Start Merging");
-    let mut dir_entry = fs::read_dir(out_folder).await?;
 
-    let mut file_paths = Vec::with_capacity(divisor);
-
-    while let Some(file_chunk) = dir_entry.next_entry().await? {
-        let path_file = file_chunk.path();
-
-        if path_file.is_file() {
-            let ext = path_file.extension();
-            if ext.is_none() {
-                file_paths.push(path_file);
-            }
-        }
-    }
-
-    file_paths.sort_by_key(|p| {
-        let num = p
-            .file_name()
-            .and_then(|os_str| os_str.to_str())
-            .and_then(|str| str.parse::<u32>().ok())
-            .expect("ERROR : need file to be number");
-
-        num
-    });
+    let file_paths = sorting(out_folder, divisor).await?;
 
     let output_file = parent.join(output_name);
     let final_file = get_available_filename(output_file);
